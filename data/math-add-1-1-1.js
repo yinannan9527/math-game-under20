9a7de20fbbc7a3e4cedd82c808c757bab7c3e56f
yinannan9527 <s10212076@gmail.com>
2025-04-21 22:58:56 +0800

    Add files via upload

---
+++ data/math-add-1-1-1.js
@@ -0,0 +1,66 @@
+﻿//
+//開始比賽前, 說明對話框的說明文字
+//
+helpText = "請找出算式的答案。大家都按GO以後開始PK";
+
+// 不要固定設定 operator，我們改成每題隨機決定加減
+
+// 題目數字設定
+numberMin = 1;
+numberMax = 19;  // 為了保證答案在 20 以內
+
+//基本設定
+optionsTotal = 6;		//共有幾個選項
+optionColTotal = 3;		//一列有幾個選項
+
+/******************************************************************
+ 以下為程式碼, 不要更動
+ ******************************************************************/
+getOneQuestion = function(tools) {
+	var question, answer, n1, n2, opSymbol, ngMin, ngOffset;
+
+	// 隨機決定是加法或減法
+	var isAddition = Math.random() < 0.5;  // 50%加法 50%減法
+
+	if (isAddition) {
+		// 【進位加法題】讓 n1 + n2 必定超過 10，但不超過 20
+		n1 = Math.floor(Math.random() * 9) + 2;  // 2~10
+		n2 = Math.floor(Math.random() * (20 - n1 - 1)) + 1;  // 讓答案 < 20
+		answer = n1 + n2;
+		opSymbol = '＋';
+	} else {
+		// 【退位減法題】讓 n1 - n2 必定 < 10 且要退位
+		// 先決定答案（0~9），然後讓 n2 = 個位數大於答案（模擬借位）
+		answer = Math.floor(Math.random() * 10);  // 答案 0~9
+		n2 = Math.floor(Math.random() * 9) + 1;    // 1~9
+		n1 = answer + n2;
+		opSymbol = '－';
+	}
+
+	question = n1 + ' ' + opSymbol + ' ' + n2 + ' = ?';
+
+	// 選項產生
+	if (answer - 4 > 0) {
+		ngMin = -4;
+	} else {
+		ngMin = 1 - answer;
+	}
+	ngOffset = 11;
+	var nRandom = tools.makeRandomIndex(ngMin, ngMin + ngOffset);
+	for (var j = 0; j < nRandom.length; j++) {
+		if (nRandom[j] == 0) {
+			nRandom.splice(j, 1);
+			break;
+		}
+	}
+
+	var op = new Object();
+	op.question = question;
+	op.optionsOK = [answer];
+	op.optionsNG = [];
+
+	for (var i = 0; i < nRandom.length; i++) {
+		op.optionsNG[i] = answer + nRandom[i];
+	}
+	return op;
+}
---
+++ data/math-add-1-1.js
@@ -0,0 +1,97 @@
+﻿//
+//開始比賽前, 說明對話框的說明文字
+//
+helpText = "請找出算式的答案。大家都按GO以後開始PK";
+
+
+//number1 operator number2 = ?
+number1Min = 1;  //number1 最小
+number1Max = 19;  //number1 最大
+
+number2Min = 1;  //number2 最小
+number2Max = 19;  //number2 最大
+
+operator = '+';  //運算子
+
+
+//基本設定
+optionsTotal = 6;		//共有幾個選項
+optionColTotal = 3;		//一列有幾個選項
+
+/******************************************************************
+ 以下為程式碼, 不要更動
+ ******************************************************************/
+getOneQuestion = function(tools) {
+	var question, 
+		answer,
+		n1,
+		n2,
+		offset,
+		r,
+		temp,
+		opSymbol,
+		ngMin,
+		ngOffset;
+		
+	
+	
+	// 抽出 number1
+        n1 = Math.floor(Math.random() * (number1Max - number1Min + 1)) + number1Min;
+
+        // 根據 n1 動態限制 n2 的最大值，使得 n1 + n2 <= 20
+        let maxN2 = Math.min(number2Max, 20 - n1);
+        if (maxN2 < number2Min) maxN2 = number2Min;  // 避免 n2 無法抽出
+
+        // 抽出 number2
+        n2 = Math.floor(Math.random() * (maxN2 - number2Min + 1)) + number2Min;
+
+
+
+	//計算答案
+	if(operator=='+') {
+		opSymbol = '＋';
+		answer = n1+n2;	//答案
+	} else if(operator=='-') {
+		opSymbol = '－';
+		if(n1-n2<0) {
+			temp = n1;
+			n1 = n2;
+			n2 = temp;
+		}
+		answer = n1-n2;	//答案
+	}	
+	
+	//組合成題幹
+	var question = n1 +' '+opSymbol+' '+ n2 + ' = ?';		//題幹
+
+	//製作其它非正解的選項
+	//以亂數產生要由答案再加減多少
+	if(answer-4>0) {
+		ngMin = -4;
+	} else {
+		ngMin = 1-answer;
+	}
+	if(number1Max<10) {
+		ngOffset = 11;
+	} else {
+		ngOffset = 21;
+	}
+	var nRandom = tools.makeRandomIndex(ngMin, ngMin+ngOffset);	//其它選項比解答加減多少(用亂數排)
+	for(var j=0; j<nRandom.length; j++) {
+		if(nRandom[j] == 0 ) {
+			nRandom.splice(j,1);	//將是答案者去掉
+			break;
+		}
+	}
+
+	var op = new Object();
+	op.optionsOK = [];
+	op.optionsNG = [];
+	
+	op.question = question;
+	op.optionsOK[0] = answer;
+	for(var i=0; i<nRandom.length; i++) {
+		op.optionsNG[i] = answer+nRandom[i];
+	}
+	return op;
+}
